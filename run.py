3001b967a17e2c5d46ef3232011a94bcfba41811
MinuMThomas <minumarytomas@gmail.com>
2022-09-29 23:08:14 +0000

    add hangman_ game function to check  coreect alphabet or not, need to fix name error

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-
 import random
 # Hangman Game
 """ 
@@ -10,13 +9,94 @@ import random
 -if wrong continue loop until no of try
 -if all correct - break
 """
-
+#hangman graghic with different stages for number of guess 
+def hangman_image():
+    graphics = ['''
+     +---+
+        |
+        |
+        |
+        ===''', '''
+    +---+
+    O   |
+        |
+        |
+       ===''', '''
+    +---+
+    O   |
+    |   |
+        |
+       ===''', '''
+    +---+
+    O   |
+   /|   |
+       |
+      ===''', '''
+   +---+
+    O   |
+   /|\  |
+        |
+      ===''', '''
+    +---+
+    O   |
+   /|\  |
+   /    |
+       ===''', '''
+   +---+
+    O   |
+   /|\  |
+   / \  |
+       ==='''
+    ]
 
 #words list for the game
 
-words = ['happy', 'winter','dublin','buzz','ring','python','quartz','jogging','jiujitsu','queue','galaxy','fancy',
+"""game function, enter letter check it is correct or not
+   count the wrong entry , show the result"""
+
+def hangman_game():
+    word = None
+    guess = None
+
+    words_list = ['happy', 'winter','dublin','buzz','ring','python','quartz','jogging','jiujitsu','queue','galaxy','fancy',
     'frazzled','syndrome','awkward']
-game_words = random.choice(words)
+    word = random.choice(words_list)
+
+
+guessed_letters = []
+wrong_guess = []
+chances = 6
+counting_hangman = -1
+print("Let's play the game")
+print(hangman_image())
+while chances > 0:
+        output = ''
+for alphabet in word:
+    if alphabet in guessed_letters:
+        output += alphabet
+    else:
+        output += '_'    
+if output == word:
+    break
+      
+guess = input('please guess a letter or word: ').lower()
+
+if guess in wrong_guess:
+        print('you already guessed letter',guess)
+            
+elif guess in word and guess.lower():
+            guessed_letters.append(guess)
+       
+else:
+        counting_hangman = counting_hangman + 1
+        chances = chances - 1
+        wrong_guess.append(guess)
+        print(hangman_image[counting_hangman])
+if chances > 0:
+        print('correct word')
+else:
+        print('sorry wrong word, try again')                
+
 
 """begin the game with option to start and exit with wecome message"""
 def welcome_message():
@@ -45,7 +125,7 @@ def game_instructions():
                 '- Good Luck!')
                 yes_input = input('press enter to begin the game')
                 if yes_input == '':
-                    game()
+                    hangman_game()
                 else:
                     print('sorry you pressed wrong key')
         else:
@@ -54,11 +134,7 @@ def game_instructions():
             break       
 game_instructions()
 
-"""game function, enter letter check it is correct or not
-   count the wrong entry , show the result"""
 
-def game():
-    
 
 
 
