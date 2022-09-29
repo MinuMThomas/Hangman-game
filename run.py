3a3ae2d9dde1e23a31d0d063191923671a74032d
MinuMThomas <minumarytomas@gmail.com>
2022-09-29 10:08:31 +0000

    initial commit add welcome message

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Hangman Game
+""" 
+- begin the game with options - start , exit
+- words list
+- get a guess
+- try few guess
+-check guess
+-if wrong continue loop until no of try
+-if all correct - break
+"""
+import random
+
+#words list for the game
+
+words = ['happy', 'winter','dublin','buzz','ring','python','quartz','jogging','jiujitsu','queue','galaxy','fancy',
+    'frazzled','syndrome','awkward']
+game_words = random.choice(words)
+
+"""begin the game with option to start and exit with wecome message"""
+def welcome_message():
+    while True:
+        name = input('Please enter your Name:\n')
+        print(f'Hello {name}, Welcome to Hangman Game! \n')
+        break
+    return name
+welcome_message()
+
